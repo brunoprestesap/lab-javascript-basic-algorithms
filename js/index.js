509453a0a4db46c6c8cd6a6175a0ba8d00d01ca9
Bruno Prestes <brunoasprestes@gmail.com>
2022-10-19 14:14:44 -0300

    Iteration 1 concluida

--- js/index.js
+++ js/index.js
@@ -1,5 +1,15 @@
 // Iteration 1: Names and Input
-//
+
+let hacker1 = " Bruno Alexandre Soares Prestes"
+let hacker2 = "Pamela Prestes"
+
+let message1 = `The driver's name is ${hacker1}`
+let message2= `The navigator's name is ${hacker2}`
+
+console.log(message1)
+console.log(message2)
+
+
 // Iteration 2: Conditionals
 
 
