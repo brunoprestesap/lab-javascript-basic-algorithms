18aee006a0e6f24bae37600163fa80d4d5f0e3c1
Bruno Prestes <brunoasprestes@gmail.com>
2022-10-19 14:25:38 -0300

    Iteration 2 concluida

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // Iteration 1: Names and Input
 
-let hacker1 = " Bruno Alexandre Soares Prestes"
+let hacker1 = "Bruno Alexandre Soares Prestes"
 let hacker2 = "Pamela Prestes"
 
 let message1 = `The driver's name is ${hacker1}`
@@ -11,6 +11,14 @@ console.log(message2)
 
 
 // Iteration 2: Conditionals
+
+if (hacker1.length > hacker2.length) {
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters.`)
+} else if (hacker1.length < hacker2.length) {
+    console.log(`It seems that the o navigator has the longest name, it has ${hacker2.length} characters.`)
+} else {
+    console.log(`Wow, you both have equally long names, ${hacker1.length} characters!.`)
+}
 
 
 // Iteration 3: Loops
